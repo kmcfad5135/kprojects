448cf3f1a496d4c5ea72c5c3abd46b53e9fa15df
kmcfad5135 <kendall.mcfadden@howardcc.edu>
2025-03-31 22:46:13 -0400

    Kodeco Chat

---
+++ app/src/main/java/com/kodeco/chat/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.kodeco.chat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kodeco.chat.ui.theme.KodecoChatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KodecoChatTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //add your own name
+                    Greeting(name = "Kendall")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KodecoChatTheme {
+        Greeting("Kendall")
+    }
+}
